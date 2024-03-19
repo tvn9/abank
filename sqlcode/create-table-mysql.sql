56531c3899642c774783aafb5249a83c95205e65
Thanh Nguyen <tv.nguyen9@outlook.com>
2024-03-18 20:00:11 -0700

    Project progress code

--- sqlcode/create-table-mysql.sql
+++ sqlcode/create-table-mysql.sql
@@ -1,39 +1,42 @@
-CREATE TABLE `accounts` (
-  `id` bigserial PRIMARY KEY,
-  `owner` varchar(255) NOT NULL,
-  `balance` bigint NOT NULL,
-  `currency` varchar(255) NOT NULL,
-  `created_at` timestampz NOT NULL DEFAULT (now())
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE `entries` (
-  `id` bigserial PRIMARY KEY,
-  `account_id` bigint,
-  `amount` bigint NOT NULL COMMENT 'can be nagative or positive',
-  `created_at` timestampz NOT NULL DEFAULT (now())
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE `transfers` (
-  `id` bigserial PRIMARY KEY,
-  `from_account_id` bigint,
-  `to_account_id` bigint,
-  `amount` bigint,
-  `status` varchar(255) NOT NULL COMMENT 'must be positive',
-  `created_at` timestampz NOT NULL DEFAULT (now())
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE INDEX `accounts_index_0` ON `accounts` (`owner`);
+CREATE INDEX ON "accounts" ("owner");
 
-CREATE INDEX `entries_index_1` ON `entries` (`account_id`);
+CREATE INDEX ON "entries" ("account_id");
 
-CREATE INDEX `transfers_index_2` ON `transfers` (`from_account_id`);
+CREATE INDEX ON "transfers" ("from_account_id");
 
-CREATE INDEX `transfers_index_3` ON `transfers` (`to_account_id`);
+CREATE INDEX ON "transfers" ("to_account_id");
 
-CREATE INDEX `transfers_index_4` ON `transfers` (`from_account_id`, `to_account_id`);
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-ALTER TABLE `entries` ADD FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
+COMMENT ON COLUMN "entries"."amount" IS 'can be nagative or positive';
 
-ALTER TABLE `transfers` ADD FOREIGN KEY (`from_account_id`) REFERENCES `accounts` (`id`);
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
 
-ALTER TABLE `transfers` ADD FOREIGN KEY (`to_account_id`) REFERENCES `accounts` (`id`);
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
